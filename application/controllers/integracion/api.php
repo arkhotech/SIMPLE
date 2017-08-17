50791bb83161c6fb8b554c6e13d51069e8b7b41b
jperezdearce <jperezdearce@arkhotech.com>
2017-08-17 10:50:00 -0300

    desarrollo de continuar

--- application/controllers/integracion/api.php
+++ application/controllers/integracion/api.php
@@ -109,7 +109,6 @@ class API extends MY_BackendController {
                 break;
             default:
                 header("HTTP/1.1 405 Metodo no permitido");
-                //show_error("405 Metodo no permitido",405, "El metodo no esta implementado" );
         }
         
     }
@@ -135,11 +134,10 @@ class API extends MY_BackendController {
        exit;
     }
     
-    private function iniciarProceso($proceso_id,$id_tarea,$body){
+    private function iniciarProceso($proceso_id, $id_tarea, $body){
         //validar la entrada
         
         if($proceso_id == NULL || $id_tarea == NULL){
-            //show_error("400 Bad Request",400, "Uno de los parametros de entrada no ha sido especificado" );
             header("HTTP/1.1 400 Bad Request");
             return;
         }
@@ -151,20 +149,28 @@ class API extends MY_BackendController {
             $tramite = new Tramite();
             $tramite->iniciar($proceso_id);
 
-            //llevar a etapas/ejecutar_form + número de tarea y proceso
-            //Se debe seleccionar la etapa que se quiere iniciar.
+            log_message("INFO", "Iniciando trámite: ".$proceso_id, FALSE);
+
             $etapa = $tramite->getEtapasActuales()->get(0)->id;
-            $nextStep = $this->ejecutarEntrada($etapa,$input,true);
+
+            $respuesta = $this->ejecutar_form($etapa, 0, $input["data"]);
+            log_message("INFO", "Respuesta de ejecutar inicio: ".$this->varDump($respuesta), FALSE);
+            $nextStep = $respuesta["id_prox_paso"];
+            $etapa_id = $respuesta["id_etapa"];
+
+
             $integrador = new FormNormalizer();
-            //echo $proceso_id, $id_tarea, $nextStep;die;
+            log_message("INFO", "Obteniendo proximo formulario para proceso_id, id_tarea, nextStep: ".$proceso_id." ".$id_tarea." ".$nextStep, FALSE);
             $nextForm = $integrador->obtenerFormularios($proceso_id, $id_tarea, $nextStep);
             $this->registrarCallbackURL($input['callback'],$etapa);
-            
+
             //validaciones etapa vencida, si existe o algo por el estilo
+
              $response = array(
                 "idInstancia" => $tramite->id,
                 "codigoRetorno" => 0,
                 "descRetorno" => "",
+                 "idEtapa" => $etapa_id,
                 "idProximoPaso" => $nextStep,
                 "output" => array(),
                 "proximoFormulario" => $nextForm
@@ -176,15 +182,42 @@ class API extends MY_BackendController {
       
     }
     
-    private function continuarProceso($idProceso,$idEtapa=NULL, $body){
-        $data = json_decode($body,true);
-         $response = array(
-           "codigoRetorno" => 0,
-           "descRetorno" => "Problemas para iniciar",
-           "idProximoPaso" => $idProceso + 1,
-           "proximoFormulario" => $data['data']
-           );
-         $this->responseJson($response);
+    private function continuarProceso($proceso_instance_id,$id_tarea=NULL, $body){
+
+        log_message("INFO", "En continuar proceso, input data: ".$body);
+
+        if($proceso_instance_id == NULL || $id_tarea == NULL){
+            header("HTTP/1.1 400 Bad Request");
+            return;
+        }
+
+        try{
+            $input = json_decode($body,true);
+
+            $tramite = new Tramite();
+            $tramite->iniciar($proceso_instance_id);
+
+            //llevar a etapas/ejecutar_form + número de tarea y proceso
+            //Se debe seleccionar la etapa que se quiere iniciar.
+            $etapa = $tramite->getEtapasActuales()->get(0)->id;
+            $nextStep = $this->ejecutarEntrada($etapa,$input,true);
+            $integrador = new FormNormalizer();
+            $nextForm = $integrador->obtenerFormularios($proceso_instance_id, $id_tarea, $nextStep);
+            $this->registrarCallbackURL($input['callback'],$etapa);
+
+            //validaciones etapa vencida, si existe o algo por el estilo
+            $response = array(
+                "codigoRetorno" => 0,
+                "descRetorno" => "",
+                "idProximoPaso" => $nextStep,
+                "output" => array(),
+                "proximoFormulario" => $nextForm
+            );
+            $this->responseJson($response);
+        }catch(Exception $e){
+            $e->getTrace();
+        }
+
     }
     
     private function generarEspecificacion($operacion,$id_tramite=NULL,$id_tarea=NULL,$id_paso = NULL){
@@ -248,6 +281,9 @@ class API extends MY_BackendController {
         $etapa = Doctrine::getTable('Etapa')->find($etapa_id);
         //obtener siempre el primer paso de la secuencia
         $paso = $etapa->getPasoEjecutable(0);
+
+        log_message("INFO", "Primer paso: ".$paso->id, FALSE);
+
         $formulario = $paso->Formulario;
         $modo = $paso->modo;
 
@@ -276,11 +312,14 @@ class API extends MY_BackendController {
             }
         }
 
+        log_message("INFO", "Guardando etapa: ".$etapa_id, FALSE);
         $etapa->save();
 
+        log_message("INFO", "Finalizando paso", FALSE);
         $etapa->finalizarPaso($paso);
        //Traer el siguiente formualrio
-        $prox_paso = $etapa->getPasoEjecutable(1); 
+        $prox_paso = $etapa->getPasoEjecutable(1);
+        log_message("INFO", "Proximo paso: ".$prox_paso->id, FALSE);
         return $prox_paso->id;
 
     }
@@ -311,7 +350,111 @@ class API extends MY_BackendController {
 
         redirect('etapas/inbox');
     }
-    
+
+    /**
+     * @param $etapa_id
+     * @param $secuencia
+     * @param $data
+     */
+    public function ejecutar_form($etapa_id, $secuencia, $data) {
+
+        log_message('info', 'ejecutar_form ($etapa_id [' . $etapa_id . '], $secuencia [' . $secuencia . '])');
+
+        $etapa = Doctrine::getTable('Etapa')->find($etapa_id);
+
+        log_message('info', 'Recupera etapa', FALSE);
+
+        if (!$etapa->pendiente) {
+            header("HTTP/1.1 100 Esta etapa ya fue completada");
+            exit;
+        }
+        if (!$etapa->Tarea->activa()) {
+            header("HTTP/1.1 101 Esta etapa no se encuentra activa");
+            exit;
+        }
+        if ($etapa->vencida()) {
+            header("HTTP/1.1 102 Esta etapa se encuentra vencida");
+            exit;
+        }
+
+        log_message('info', 'Recupera paso ejecutable', FALSE);
+
+        $paso = $etapa->getPasoEjecutable($secuencia);
+        $formulario = $paso->Formulario;
+
+            $validar_formulario = FALSE;
+            log_message('info', 'Validando formulario', FALSE);
+            foreach ($formulario->Campos as $c) {
+                // Validamos los campos que no sean readonly y que esten disponibles (que su campo dependiente se cumpla)
+                if ($c->isEditableWithCurrentPOST($etapa_id)) {
+                    $c->formValidate($etapa->id);
+                    $validar_formulario = TRUE;
+                }
+            }
+            log_message('info', 'Resultado validacion: '.$validar_formulario, FALSE);
+            if (true){//!$validar_formulario){//} || $this->form_validation->run() == TRUE) {
+                log_message('info', 'Entra en if', FALSE);
+                // Almacenamos los campos
+                foreach ($formulario->Campos as $c) {
+                    log_message('info', 'Recorriendo campos: '.$c->nombre, FALSE);
+                    // Almacenamos los campos que no sean readonly y que esten disponibles (que su campo dependiente se cumpla)
+
+                    log_message('info', 'Chequea si es editable', FALSE);
+                    if ($c->isEditableWithCurrentPOST($etapa_id)) {
+                        log_message('info', 'lo es', FALSE);
+                        $dato = Doctrine::getTable('DatoSeguimiento')->findOneByNombreAndEtapaId($c->nombre, $etapa->id);
+                        if (!$dato)
+                            $dato = new DatoSeguimiento();
+                        $dato->nombre = $c->nombre;
+                        log_message('info', 'valor: '.$data[$c->nombre], FALSE);
+                        $dato->valor = $data[$c->nombre]=== false?'' :  $data[$c->nombre];
+
+                        if (!is_object($dato->valor) && !is_array($dato->valor)) {
+                            if (preg_match('/^\d{4}[\/\-]\d{2}[\/\-]\d{2}$/', $dato->valor)) {
+                                $dato->valor=preg_replace("/^(\d{4})[\/\-](\d{2})[\/\-](\d{2})/i", "$3-$2-$1", $dato->valor);
+                            }
+                        }
+
+                        $dato->etapa_id = $etapa->id;
+                        $dato->save();
+                    }
+                }
+                log_message('info', 'Termina con datos', FALSE);
+                $etapa->save();
+
+                log_message('info', 'Guarda etapa', FALSE);
+                $etapa->finalizarPaso($paso);
+
+                log_message('info', 'Finaliza paso', FALSE);
+
+                $prox_paso = $etapa->getPasoEjecutable($secuencia + 1);
+                if (!$prox_paso) {
+                    log_message('info', 'No hay proximo paso se avanza a la sigeouinte etapa', FALSE);
+                    $etapa->avanzar();
+                    log_message('info', 'Etapa siguiente: '.$etapa->id, FALSE);
+                } else if ($etapa->Tarea->final && $prox_paso->getReadonly() && end($etapa->getPasosEjecutables()) == $prox_paso) { //Cerrado automatico
+                    log_message('info', 'Fibnal proceso, se procesa el cierre automático', FALSE);
+                    $etapa->iniciarPaso($prox_paso);
+                    $etapa->finalizarPaso($prox_paso);
+                    $etapa->avanzar();
+                } else {
+                    log_message('info', 'Hay proximo paso y no es etapa final', FALSE);
+                    //$respuesta->redirect = site_url('etapas/ejecutar/' . $etapa_id . '/' . ($secuencia + 1)) . ($qs ? '?' . $qs : '');
+                }
+            } else {
+                header("HTTP/1.1 500 ".validation_errors());
+                exit;
+            }
+
+        $respuesta = array(
+            "id_prox_paso" => $prox_paso->id,
+            "id_etapa" => $etapa->id
+        );
+
+        return $respuesta;
+    }
+
+
     function throwError($numero,$mensaje){
         $errorcodes = array(
 							200	=> 'OK',
