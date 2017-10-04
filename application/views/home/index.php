e69337d710c732b90204d2c4ac168b5189b324ba
Jimmy M <jmunoz@arkhotech.com>
2017-10-04 10:25:49 -0300

    se integra diseño en home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -7,7 +7,9 @@
 <html class="no-js" lang=""> <!--<![endif]-->
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="language" content="es">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <title><?=Cuenta::cuentaSegunDominio()!='localhost'?Cuenta::cuentaSegunDominio()->nombre_largo:'SIMPLE'?> - <?= $title ?></title>
     <!--[if IE]><link rel="shortcut icon" href="/favicon.ico"><![endif]-->
     <link rel="icon" href="/favicon.png">
@@ -15,14 +17,32 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="<?= base_url() ?>assets/home/css/apple-touch-icon.png" rel="apple-touch-icon">
-    <link href="<?= base_url() ?>assets/home/css/gobstrap.min.css" rel="stylesheet">
-    <link href="<?= base_url() ?>assets/home/css/gobstrap-theme.min.css" rel="stylesheet">
-    <link href="<?= base_url() ?>assets/home/css/main.css" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/newhome/css/style.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/newhome/css/components.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/newhome/css/prism-min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/newhome/css/main.css" rel="stylesheet">
 
-    <!-- <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script> -->
+    <script type="text/javascript">
+      var site_key = "<?= sitekey() ?>";
+
+      var onloadCallback = function() {
+        if ($('#login_captcha').length && '<?=$this->session->flashdata('login_erroneo')?>' == 'TRUE') {
+          grecaptcha.render('login_captcha', {
+            'sitekey' : site_key
+          });
+        }
+
+        if ($('#form_captcha').length) {
+          grecaptcha.render("form_captcha", {
+            sitekey : site_key
+          });
+        }
+      };
+    </script>
   </head>
+
   <body>
     <!--[if lt IE 8]>
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
@@ -35,56 +55,51 @@
               <img class="logo" src="<?= Cuenta::cuentaSegunDominio()!='localhost' ? Cuenta::cuentaSegunDominio()->logoADesplegar : base_url('assets/img/logo.png') ?>" alt="<?= Cuenta::cuentaSegunDominio()!='localhost' ? Cuenta::cuentaSegunDominio()->nombre_largo : 'Simple' ?>" />
             </a>
             <div class="titleComuna">
-              <h1><?= Cuenta::cuentaSegunDominio()!='localhost' ? Cuenta::cuentaSegunDominio()->nombre_largo : '' ?></h1>
-              <p><?= Cuenta::cuentaSegunDominio()!='localhost' ? Cuenta::cuentaSegunDominio()->mensaje : '' ?></p>
+              <h1><?= Cuenta::cuentaSegunDominio() != 'localhost' ? Cuenta::cuentaSegunDominio()->nombre_largo : '' ?></h1>
+              <p><?= Cuenta::cuentaSegunDominio() != 'localhost' ? Cuenta::cuentaSegunDominio()->mensaje : '' ?></p>
             </div>
           </div>
+          
           <div class="navbar-right">
             <div class="btn-group" role="group">
+            <div class="vr"></div>
               <ul id="userMenu" class="nav nav-pills pull-right">
                 <?php if (!UsuarioSesion::usuario()->registrado): ?>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle btn" data-toggle="dropdown" href="#">Iniciar sesi&oacute;n<span class="caret"></span></a>
-                        <ul class="dropdown-menu pull-right">
-                            <li id="loginView">
-                                <div class="simple">
-                                    <div class="wrapper">
-                                <form method="post" class="ajaxForm" action="<?= site_url('autenticacion/login_form') ?>">        
-                                    <fieldset>
-                                        <div class="validacion"></div>
-                                        <input type="hidden" name="redirect" value="<?= current_url() ?>" />
-                                        <label for="usuario">Usuario o Correo electrónico</label>
-                                        <input name="usuario" id="usuario" type="text" class="input-xlarge">
-                                        <label for="password">Contraseña</label>
-                                        <input name="password" id="password" type="password" class="input-xlarge">
-                                        <p class="olvido"><a href="<?= site_url('autenticacion/olvido') ?>">¿Olvidaste tu contraseña?</a> - <a href="<?= site_url('autenticacion/registrar') ?>">Registrate aquí</a></p>
-                                        <button class="btn btn-primary pull-right" type="submit">Ingresar</button>
-                                    </fieldset>
-                                </form>
-                                </div>
-                                </div>
-                                <!-- Modificable 1: Acceso de Identificación electronica -->
-                                <div class="claveunica">
-                                    <div class="wrapper">
-                                    <?php //if(!$claveunicaOnly):?><p>O utilice ClaveÚnica</p><?php //endif ?>
-                                    <a href="<?= site_url('autenticacion/login_openid?redirect=' . current_url()) ?>"><img src="<?= base_url() ?>assets/img/claveunica-medium.png" alt="OpenID"/></a>
-                                    </div>
-                                </div>
-                                <!-- Fin Modificable 1 -->  
-                            </li>
-                        </ul>
-                    </li>
+                  <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Iniciar sesi&oacute;n<span class="caret"></span></a>
+                    <ul class="dropdown-menu pull-right">
+                      <li id="loginView">
+                        <div class="simple">
+                          <div class="wrapper">
+                            <form method="post" class="ajaxForm" action="<?= site_url('autenticacion/login_form') ?>">        
+                              <div class="validacion"></div>
+                              <input type="hidden" name="redirect" value="<?= current_url() ?>" />
+                              <label for="usuario">Usuario o Correo electrónico</label>
+                              <input name="usuario" id="usuario" type="text" class="input-xlarge">
+                              <label for="password">Contraseña</label>
+                              <input name="password" id="password" type="password" class="input-xlarge">
+                              <div id="login_captcha"></div>
+                              <button class="button button--red" type="submit" style="float: right; cursor: pointer;">Ingresar</button>
+                              <a href="<?= site_url('autenticacion/login_openid?redirect=' . current_url()) ?>" class="link" style="float: right;">Clave Única</a>
+                              <div style="clear:both;"></div>
+                            </form>
+                            <a href="<?= site_url('autenticacion/olvido') ?>" class="link" ">¿Olvidaste tu contraseña?</a>
+                          </div>
+                        </div>
+                      </li>
+                    </ul>
+                  </li>
                 <?php else: ?>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Bienvenido/a <?= UsuarioSesion::usuario()->displayName() ?><span class="caret"></span></a>
-                        <ul class="dropdown-menu btn" style="padding: 0px !important;">
-                            <?php if (!UsuarioSesion::usuario()->open_id): ?> 
-                                <li><a href="<?= site_url('cuentas/editar') ?>"><i class="icon-user"></i> Mi cuenta</a></li>
-                            <?php endif; ?>
-                            <?php if (!UsuarioSesion::usuario()->open_id): ?><li><a href="<?= site_url('cuentas/editar_password') ?>"><i class="icon-lock"></i> Cambiar contraseña</a></li><?php endif; ?>
-                            <li><a href="<?= site_url('autenticacion/logout') ?>"><i class="icon-off"></i> Cerrar sesión</a></li>
-                        </ul>
-                    </li>
+                  <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Bienvenido/a <?= UsuarioSesion::usuario()->displayName() ?><span class="caret"></span></a>
+                    <ul class="dropdown-menu btn" style="padding: 0px !important;">
+                      <?php if (!UsuarioSesion::usuario()->open_id): ?> 
+                        <li><a href="<?= site_url('cuentas/editar') ?>"><i class="icon-user"></i> Mi cuenta</a></li>
+                      <?php endif; ?>
+                      <?php if (!UsuarioSesion::usuario()->open_id): ?><li><a href="<?= site_url('cuentas/editar_password') ?>"><i class="icon-lock"></i> Cambiar contraseña</a></li><?php endif; ?>
+                      <li><a href="<?= site_url('autenticacion/logout') ?>"><i class="icon-off"></i> Cerrar sesión</a></li>
+                    </ul>
+                  </li>
                 <?php endif; ?>
               </ul>
             </div>
@@ -92,8 +107,8 @@
         </div>
       </div>
     </header>
-    
-    <main style="min-height:480px">
+
+    <main class="main">
       <div class="container">
         <div class="row">
           <div class="col-sm-12">
@@ -101,39 +116,38 @@
               <section id="simple-destacados">
                   <div class="section-header">
                     <?php if($sidebar == 'disponibles'):?>
-                      <h2>Trámites destacados</h2>
+                      <h2>Tr&aacute;mites destacados</h2>
                     <?php else: ?>
-                      <h2>Trámites - <?= $categoria->nombre ?></h2>
-                      <a href="<?=site_url('home/index/')?>" class="btn btn-primary preventDoubleRequest" style="float:right">
+                      <h2>Tr&aacute;mites - <?= $categoria->nombre ?></h2>
+                      <a href="<?=site_url('home/index/')?>" class="btn btn-primary preventDoubleRequest" style="float: right;">
                         <i class="icon-file icon-white"></i> Volver
                         </a>
                     <?php endif ?>
-                    <div class="line"></div>
                   </div>
                   <div class="row">
                       <?php foreach ($procesos as $p): ?>
                         <?php if($p->destacado == 1 || $sidebar == 'categorias'):?>
                           <div class="col-md-4 item">
                             <div class="tarjeta">
-                                <?php if($p->icon_ref):?>
-                                  <div class="text-left">
-                                    <img src="<?= base_url('assets/img/icons/' . $p->icon_ref) ?>" class="img-service">
-                                  </div>
-                                <?php else:?>
-                                  <div class="text-left">
-                                    <img src="<?= base_url('assets/img/icons/nologo.png') ?>" class="img-service">
-                                  </div>
-                                <?php endif ?>
+                              <?php if($p->icon_ref):?>
+                                <div class="text-left">
+                                  <img src="<?= base_url('assets/img/icons/' . $p->icon_ref) ?>" class="img-service">
+                                </div>
+                              <?php else:?>
+                                <div class="text-left">
+                                  <img src="<?= base_url('assets/img/icons/nologo.png') ?>" class="img-service">
+                                </div>
+                              <?php endif ?>
                               <h4><?= $p->nombre ?></h4>
-                              <div class="btn_derecha">
+                              <div class="enlace_cat_proc">
                                 <?php if($p->canUsuarioIniciarlo(UsuarioSesion::usuario()->id)):?>
-                                <a href="<?=site_url('tramites/iniciar/'.$p->id)?>" class="btn btn-primary preventDoubleRequest"><i class="icon-file icon-white"></i> Iniciar</a>
+                                  <a href="<?=site_url('tramites/iniciar/'.$p->id)?>"><i class="icon-file icon-white"></i> Iniciar</a>
                                 <?php else: ?>
-                                    <?php if($p->getTareaInicial()->acceso_modo=='claveunica'):?>
-                                    <a href="<?=site_url('autenticacion/login_openid')?>?redirect=<?=site_url('tramites/iniciar/'.$p->id)?>"><img style="max-width: none;" src="<?=base_url('assets/img/claveunica-medium.png')?>" alt="ClaveUnica" /></a>
-                                    <?php else:?>
-                                    <a href="<?=site_url('autenticacion/login')?>?redirect=<?=site_url('tramites/iniciar/'.$p->id)?>" class="btn btn-primary"><i class="icon-white icon-off"></i> Autenticarse</a>
-                                    <?php endif ?>
+                                  <?php if($p->getTareaInicial()->acceso_modo=='claveunica'):?>
+                                  <a href="<?=site_url('autenticacion/login_openid')?>?redirect=<?=site_url('tramites/iniciar/'.$p->id)?>"><i class="icon-white icon-clave-unica"></i> Clave &Uacute;nica</a>
+                                  <?php else:?>
+                                  <a href="<?=site_url('autenticacion/login')?>?redirect=<?=site_url('tramites/iniciar/'.$p->id)?>">Autenticarse</a>
+                                  <?php endif ?>
                                 <?php endif ?>
                               </div>
                             </div>
@@ -149,7 +163,6 @@
               <section id="simple-categorias">
                   <div class="section-header">
                     <h2>Categor&iacute;as</h2>
-                    <div class="line"></div>
                   </div>
                   <div class="row">
                     <?php foreach ($categorias as $c): ?>
@@ -160,7 +173,7 @@
                               <?php if($c->icon_ref):?>
                                 <img src="<?= base_url('uploads/logos/' . $c->icon_ref) ?>" class="img-service">
                               <?php else:?>
-                                  <img src="<?= base_url('assets/img/icons/nologo.png') ?>" class="img-service">
+                                <img src="<?= base_url('assets/img/icons/nologo.png') ?>" class="img-service">
                               <?php endif ?>
                             </div>
                             <span class="title"><?= $c->nombre ?></span>
@@ -176,7 +189,7 @@
             <?php if ($num_otros > 0 && $sidebar != 'categorias'): ?>
             <section id="simple-destacados">
                 <div class="section-header">
-                  <h2 style="width:40%">Otros trámites</h2>
+                  <h2>Otros trámites</h2>
                   <div class="line"></div>
                 </div>
                 <div class="row">
@@ -194,14 +207,14 @@
                               </div>
                             <?php endif ?>
                           <h4><?= $p->nombre ?></h4>
-                          <div class="btn_derecha">
-                            <?php if($p->canUsuarioIniciarlo(UsuarioSesion::usuario()->id)):?>
-                            <a href="<?=site_url('tramites/iniciar/'.$p->id)?>" class="btn btn-primary preventDoubleRequest"><i class="icon-file icon-white"></i> Iniciar</a>
+                          <div class="enlace_cat_proc">
+                            <?php if ($p->canUsuarioIniciarlo(UsuarioSesion::usuario()->id)): ?>
+                            <a href="<?=site_url('tramites/iniciar/'.$p->id)?>"><i class="icon-file icon-white"></i> Iniciar</a>
                             <?php else: ?>
-                                <?php if($p->getTareaInicial()->acceso_modo=='claveunica'):?>
-                                <a href="<?=site_url('autenticacion/login_openid')?>?redirect=<?=site_url('tramites/iniciar/'.$p->id)?>"><img style="max-width: none;" src="<?=base_url('assets/img/claveunica-medium.png')?>" alt="ClaveUnica" /></a>
-                                <?php else:?>
-                                <a href="<?=site_url('autenticacion/login')?>?redirect=<?=site_url('tramites/iniciar/'.$p->id)?>" class="btn btn-primary"><i class="icon-white icon-off"></i> Autenticarse</a>
+                                <?php if ($p->getTareaInicial()->acceso_modo == 'claveunica'): ?>
+                                <a href="<?=site_url('autenticacion/login_openid')?>?redirect=<?=site_url('tramites/iniciar/'.$p->id)?>"><i class="icon-white icon-clave-unica"></i> Clave &Uacute;nica</a>
+                                <?php else: ?>
+                                <a href="<?=site_url('autenticacion/login')?>?redirect=<?=site_url('tramites/iniciar/'.$p->id)?>">Autenticarse</a>
                                 <?php endif ?>
                             <?php endif ?>
                           </div>
@@ -216,39 +229,28 @@
         </div>
     </main>
 
-    <div class="container" style="margin-top:100px;">
-      <footer class="footer-gobcl">
-        <div class="banda-bicolor">
-          <span class="azul"></span>
-          <span class="rojo"></span>
-        </div>
-
-        <div class="top">
-          <div class="listas">
-            <div class="lista">
-              <ul>
+    <footer class="site-footer">
+      <div class="container"><a class="site-footer_logo" href="#"><i class="icon-gob"></i></a>
+        <div class="row hidden-xs">
+          <div class="table-lg-row">
+            <div class="col-sm-2"></div>
+            <div class="col-sm-6">
+              <ul class="menu">
                 <li><a href="http://www.modernizacion.gob.cl/" target="_blank">Iniciativa de la Unidad de Modernizaci&oacute;n y Gobierno Digital</a></li>
                 <li><a href="http://www.minsegpres.gob.cl/" target="_blank">Ministerio Secretaría General de la Presidencia</a></li>
               </ul>
             </div>
           </div>
         </div>
-
-        <div class="bottom">
-          <div class="clearfix"></div>
-          <div class="banda-bicolor">
-            <span class="azul"></span>
-            <span class="rojo"></span>
-          </div>
-        </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="<?= base_url() ?>assets/home/js/gobstrap.min.js"></script>
     <script src="<?= base_url() ?>assets/home/js/jquery.sticky.js"></script>
     <script src="<?= base_url() ?>assets/home/js/main.js"></script>
     <script src="<?= base_url() ?>assets/home/js/home.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit&hl=es"></script>
 
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. 
     <script>
