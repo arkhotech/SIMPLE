e15b39f9d978f99ac1b453e46d1fae088ffec5e4
jperezdearce <jperezdearce@arkhotech.com>
2017-10-12 12:48:03 -0300

    se agregan columnas a tabla proceso

---
+++ application/migrations/45.php
@@ -0,0 +1,21 @@
+<?php
+class Migration_45 extends Doctrine_Migration_Base {
+
+    public function up() {
+        $this->addColumn('proceso', 'version', 'int', 10, array('notnull'=>1,'default'=>1));
+        $this->addColumn('proceso', 'root', 'int', 10, array('notnull'=>1,'default'=>1));
+        $this->addColumn('proceso', 'publicado', 'boolean', null, array('notnull'=>1,'default'=>1));
+    }
+
+    public function postUp() {
+        $q = Doctrine_Manager::getInstance()->getCurrentConnection();
+        $q->execute("UPDATE proceso p SET p.version=1, p.root=p.id, p.publicado=1");
+    }
+
+    public function down() {
+        $this->removeColumn('proceso', 'version');
+        $this->removeColumn('proceso', 'root');
+        $this->removeColumn('proceso', 'publicado');
+    }
+}
+?>
